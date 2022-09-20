9f529889a6358f3f9c608ee87b076f131ea0103d
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-09-20 13:10:46 -0400

    Update b64decode.ps1

--- b64decode.ps1
+++ b64decode.ps1
@@ -1,3 +1,3 @@
 # replace b64 with ur payload... use this to gen the exe 
 
-[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("YmxhaGJsYWg="))
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("payloadhere"))
